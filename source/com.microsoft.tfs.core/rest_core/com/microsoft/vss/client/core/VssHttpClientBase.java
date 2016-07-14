5fac60e8f33bbc4c8cb32885c09e656a600d025f
Alex Rukhlin <arukhlin@microsoft.com>
2016-07-14 10:08:50 -0400

    Add new media type

--- source/com.microsoft.tfs.core/rest_core/com/microsoft/vss/client/core/VssHttpClientBase.java
+++ source/com.microsoft.tfs.core/rest_core/com/microsoft/vss/client/core/VssHttpClientBase.java
@@ -58,7 +58,8 @@ public abstract class VssHttpClientBase {
     protected final static String APPLICATION_JSON_TYPE = "application/json"; //$NON-NLS-1$
     protected final static String APPLICATION_OCTET_STREAM_TYPE = "application/octet-stream"; //$NON-NLS-1$
     protected final static String APPLICATION_ZIP_TYPE = "application/zip"; //$NON-NLS-1$
-    protected final static String TEXT_PLAIN_TYPE = "plain/text"; //$NON-NLS-1$
+    protected final static String TEXT_PLAIN_TYPE = "text/plaint"; //$NON-NLS-1$
+    protected final static String TEXT_HTML_TYPE = "text/html"; //$NON-NLS-1$
     protected final static String APPLICATION_JSON_PATCH_TYPE = "application/json-patch+json"; //$NON-NLS-1$
     protected final static String APPLICATION_GIT_MEDIA_TYPE = "application/vnd.git-media"; //$NON-NLS-1$
     protected final static String IMAGE_SVG_XML_MEDIA_TYPE = "image/svg+xml"; //$NON-NLS-1$
