e1acac080d7fe49116195e946298640bc6fde0f2
Ivan Migalev <ivan.migalev@jetbrains.com>
2019-11-24 00:20:22 +0700

    Add native mutex implementation for Windows
    (cherry picked from commit ca7d07c840a4548e5b417b91933f0d49166ec228)

--- source/com.microsoft.tfs.jni/src/com/microsoft/tfs/jni/internal/synchronization/NativeSynchronization.java
+++ source/com.microsoft.tfs.jni/src/com/microsoft/tfs/jni/internal/synchronization/NativeSynchronization.java
@@ -5,10 +5,15 @@ package com.microsoft.tfs.jni.internal.synchronization;
 
 import com.microsoft.tfs.jni.Synchronization;
 import com.microsoft.tfs.jni.internal.LibraryNames;
+import com.microsoft.tfs.jni.internal.winapi.Kernel32;
 import com.microsoft.tfs.jni.loader.NativeLoader;
 import com.microsoft.tfs.util.Check;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.WinNT;
 
 public class NativeSynchronization implements Synchronization {
+    private static Kernel32 kernel32 = Kernel32.INSTANCE;
+
     /**
      * This static initializer is a "best-effort" native code loader (no
      * exceptions thrown for normal load failures).
@@ -29,39 +34,52 @@ public class NativeSynchronization implements Synchronization {
     public long createMutex(final String name) {
         Check.notNull(name, "name"); //$NON-NLS-1$
 
-        return nativeCreateMutex(name);
+        WinNT.HANDLE mutex = kernel32.CreateMutex(null, false, name);
+        if (mutex == null)
+            return -1L;
+
+        return Pointer.nativeValue(mutex.getPointer());
     }
 
     @Override
     public int waitForMutex(final long mutexId, final int timeout) {
         Check.isTrue(mutexId >= 0, "mutexId >= 0"); //$NON-NLS-1$
 
-        return nativeWaitForMutex(mutexId, timeout);
+        if (mutexId == 0L)
+            return -1;
+
+        WinNT.HANDLE mutex = new WinNT.HANDLE(Pointer.createConstant(mutexId));
+        int result = kernel32.WaitForSingleObject(mutex, timeout);
+        if (result == WinNT.WAIT_ABANDONED || result == WinNT.WAIT_OBJECT_0)
+            return 1;
+
+        // Would block: return 0
+        if (result == WinNT.WAIT_TIMEOUT)
+            return 0;
+
+        // Error: return -1
+        return -1;
     }
 
     @Override
     public boolean releaseMutex(final long mutexId) {
         Check.isTrue(mutexId >= 0, "mutexId >= 0"); //$NON-NLS-1$
 
-        return nativeReleaseMutex(mutexId);
+        if (mutexId == 0L)
+            return false;
+
+        WinNT.HANDLE mutex = new WinNT.HANDLE(Pointer.createConstant(mutexId));
+        return kernel32.ReleaseMutex(mutex);
     }
 
     @Override
     public boolean closeMutex(final long mutexId) {
         Check.isTrue(mutexId >= 0, "mutexId >= 0"); //$NON-NLS-1$
 
-        return nativeCloseMutex(mutexId);
+        if (mutexId == 0L)
+            return false;
+
+        WinNT.HANDLE mutex = new WinNT.HANDLE(Pointer.createConstant(mutexId));
+        return kernel32.CloseHandle(mutex);
     }
-
-    /*
-     * Native method hooks.
-     */
-
-    private static native long nativeCreateMutex(String name);
-
-    private static native int nativeWaitForMutex(long mutexId, int timeout);
-
-    private static native boolean nativeReleaseMutex(long mutexId);
-
-    private static native boolean nativeCloseMutex(long mutexId);
 }
