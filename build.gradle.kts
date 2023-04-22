1a09f8a02b85a6a032585c71db71a60e12093eb6
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-04-22 18:43:41 +0200

    Build: import Ant project into Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,8 @@ tasks.wrapper {
     gradleVersion = "8.1.1"
     distributionType = Wrapper.DistributionType.ALL
 }
+
+buildDir = file("gradle-build")
+
+ant.properties["dir.machine.build-runtime"] = file(".build/eclipse/eclipse").path
+ant.importBuild("build/build.xml")
